808057c18d3e281c40e6ae664a82841783003798
noman <ns71445@gmail.com>
2021-12-26 16:28:38 +0500

    Work done

---
+++ form.js
@@ -0,0 +1,34 @@
+function myform(){
+    var mName = document.getElementById('name');
+    var f_name= document.getElementById('fname');
+    var rcord = document.getElementById('record');
+    var tpin = document.getElementById('topin');
+    var dt = document.getElementById('date');
+    var ntion = document.getElementById('nationality');
+    var gnder = document.getElementById('gender');
+    var rligion = document.getElementById('relogion');
+    var rsidnt = document.getElementById('res')
+    var cnic = document.getElementById('cnic');
+    var num1  =document.getElementById('num');
+    var markof = document.getElementById('mark');
+    var pocode = document.getElementById('pcode');
+    
+    document.write(mName);
+    document.write(f_name);
+    document.write(rcord);
+    document.write(tpin);
+    document.write(dt);
+    document.write(ntion);
+    document.write(gnder);
+
+    console.log(f_name)
+    console.log(rcord)
+    // console.log(topin)
+    // console.log(name)
+    // console.log(nation)
+    // console.log(gender)
+    // console.log(residnt)
+
+
+}
+console.log("asdfghjkl")
