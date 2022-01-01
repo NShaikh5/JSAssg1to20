728cb3407f961deabf9967e0535c182b728d9dee
noman <ns71445@gmail.com>
2022-01-01 17:54:51 +0500

    first commit

---
+++ ToDoApp.js
@@ -0,0 +1,11 @@
+function todo(){
+
+var user1 = document.getElementById('user')
+var a = user1.value;
+document.write(a + "<button id='edit' >Edit</button> <button id='dlt'  onclick='delet()' >Delete</button>")
+
+
+}
+function delet(){
+    console.log(dlet.clear());
+}
